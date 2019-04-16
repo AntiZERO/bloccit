8a29b3cffe92f02b80b43b4bcfdcedf7e9addf50
AntiZERO <gray2884@gmail.com>
2019-04-16 12:27:07 -0500

    WIP deply to production

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const app = require("./app");
 const http = require("http");
-const server = http.createServer(app);
+const port = normalizePort(app);
 
-server.listen(3000);
+server.listen(port);
+
+function normalizeport(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 server.on("listening", () => {
-  console.log("server is listening for requests on port 3000");
+  console.log("server is listening for requests on port ${server.address().port}");
 });
