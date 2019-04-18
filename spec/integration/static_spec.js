3547723bcaddef98e566dfc6bd737e6d4ed738c0
AntiZERO <gray2884@gmail.com>
2019-04-18 13:43:04 -0500

    assignment-2995 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -14,8 +14,18 @@ describe("routes : static", () => {
     });
   });
 
+    describe("GET /about", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
+  });
+
   describe("GET /marco", () =>{
-    it("should return status code 200 and response 'polo'", () => {
+    it("should return status code 200 and response 'polo'", (done) => {
       request.get(base + "marco", (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toBe("polo")
--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -2,7 +2,14 @@
 Each function will contain a handler for a particular route.*/
 
 module.exports = {
+
   index(req, res, next) {
     res.render("static/index", {title: "Welcome to Bloccit"});
+  },
+
+  about(req, res, next) {
+    res.render("static/about",  {title: "About Us"});
   }
+
+
 }
