9be9c65867e464994db1a7bc4d85f7d80e0ccd89
AntiZERO <gray2884@gmail.com>
2019-04-27 17:47:30 -0500

    checkpoint-3005 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -14,24 +14,4 @@ describe("routes : static", () => {
     });
   });
 
-    describe("GET /about", () => {
-    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(body).toContain("About Us");
-        done();
-      });
-    });
-  });
-
-  describe("GET /marco", () =>{
-    it("should return status code 200 and response 'polo'", (done) => {
-      request.get(base + "marco", (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toBe("polo")
-        done();
-      });
-    });
-  });
-
 });
