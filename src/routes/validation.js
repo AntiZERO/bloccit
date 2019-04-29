33c8d585b07ef39014321d7bdb46e7f006b8b2f7
AntiZERO <gray2884@gmail.com>
2019-04-29 17:46:03 -0500

    assignment completed - removed topidId param from validate topics

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -19,7 +19,6 @@ module.exports = {
     
     validateTopics(req, res, next) {
       if(req.method === "POST") {
-        req.checkParams("topicId", "must be valid").notEmpty().isInt();
         req.checkBody("title", "must be at least 2 characters in length").isLength({min: 2});
         req.checkBody("description", "must be at least 10 characters in length").isLength({min: 10});
       }
