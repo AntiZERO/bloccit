d89a1ded5273e26f16955f11db51c8950bc9977b
AntiZERO <gray2884@gmail.com>
2019-05-02 16:46:17 -0500

    multiple errors in integration/topics_spec.js

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,33 +1,44 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Topic", () => {
 
   beforeEach((done) => {
     this.topic;
     this.post;
-    sequelize.sync({force: true}).then((res) => {
-      Topic.create({
-        title: "The History of Man",
-        description: "A compilation historical facts regarding mankind."
+    this.user;
+
+    sequelize.sync({ force: true }).then((res) => {
+
+      User.create({ // Create User Object
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
       })
-      .then((topic) => {
-        this.topic = topic;
-        Post.create({
-          title: "Mankind sets foot on another world",
-          body: "In July of 1969, Neil Armstrong became the first human being to stand on the surface of another celestial body.",
-          topicId: this.topic.id
+        .then((user) => {
+          this.user = user; //store the user
+
+          Topic.create({ // Create a Topic object.
+            title: "Expeditions to Alpha Centauri",
+            description: "A compilation of reports from recent visits to the star system.",
+            posts: [{
+              title: "My first visit to Proxima Centauri b",
+              body: "I saw some rocks.",
+              userId: this.user.id
+            }]
+          }, {
+              include: {
+                model: Post,
+                as: "posts"
+              }
+            })
+            .then((topic) => {
+              this.topic = topic; //store the topic
+              this.post = topic.posts[0]; //store the post
+              done();
+            })
         })
-        .then((post) => {
-          this.post = post;
-          done();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        done();
-      });
     });
   });
 
@@ -69,7 +80,8 @@ describe("Topic", () => {
       Post.create({
         title:"Mankind harnesses the power of the atom",
         body: "The first atomic bomb was successfully detonated on July 16th, 1945, Trinity Test in New Mexico as part of the Manhatten Project",
-        topicId: this.topic.id
+        topicId: this.topic.id,
+        userId: this.user.id
       })
       .then((post) => {
         this.topic.getPosts()
---
+++ src/policies/application.js
@@ -0,0 +1,48 @@
+module.exports = class ApplicationPolicy {
+
+  // the contructor initializes the policy instance with the currently authenticated
+  // user and objects we are trying to authorize.
+   constructor(user, record) {
+     this.user = user;
+     this.record = record;
+   }
+ 
+  // `_isOwner` is a helper method and that checks that a record is present and the user owns it.
+   _isOwner() {
+     return this.record && (this.record.userId == this.user.id);
+   }
+ 
+  // `_isAdmin` is a helper method that checks that a user is present and that the user is an admin user.
+   _isAdmin() {
+     return this.user && this.user.role == "admin";
+   }
+ 
+  // `new` checks that a user is present. `create` delegates to `new`. `show` always authorizes the action.
+   new() {
+     return this.user != null;
+   }
+ 
+   create() {
+     return this.new();
+   }
+ 
+   show() {
+     return true;
+   }
+ 
+  // `edit` checks that the user is allowed to create a new record, a record is present, and either
+  // the user owns the record, or the user is an admin.
+   edit() {
+     return this.new() &&
+       this.record && (this._isOwner() || this._isAdmin());
+   }
+ 
+   update() {
+     return this.edit();
+   }
+ 
+  // `destroy` delegates to `update`
+   destroy() {
+     return this.update();
+   }
+ }
