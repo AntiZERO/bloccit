1e9e649bf10abb8e92eedc847836f6afdd4f3664
AntiZERO <gray2884@gmail.com>
2019-05-06 14:44:36 -0500

    comments checkpoint completed

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -1,39 +1,43 @@
 const Post = require("./models").Post;
+// const Topic = require("./models").Topic;
+const Comment = require("./models").Comment;
+const User = require("./models").User;
 const Authorizer = require("../policies/post.js");
 
 module.exports = {
 
-  addPost(newPost, callback){
+  addPost(newPost, callback) {
     return Post.create(newPost)
-    .then((post) => {
-      callback(null, post);
-    })
-    .catch((err) => {
-      callback(err);
-    })
+      .then((post) => {
+        callback(null, post);
+      })
+      .catch((err) => {
+        callback(err);
+      })
   },
 
-  getPost(id, callback){
-    return Post.findById(id)
-    .then((post) => {
-      callback(null, post);
+  getPost(id, callback) {
+  // Pass an options object that sequelize uses when building the query. By passing
+  // the include property, we are telling to to include all objects in the table
+  // associated witht he model Comment and set them as a value to the property
+  // comments. The nested include tells Sequelize that for each comment, also
+  // include the associated User.
+    return Post.findById(id, {
+      include: [
+        {
+          model: Comment, as: "comments", include: [
+            { model: User }
+          ]
+        }
+      ]
     })
-    .catch((err) => {
-      callback(err);
-    })
+      .then((post) => {
+        callback(null, post);
+      })
+      .catch((err) => {
+        callback(err);
+      })
   },
-
-  // deletePost(id, callback){
-  //   return Post.destroy({
-  //     where: { id }
-  //   })
-  //   .then((deletedRecordsCount) => {
-  //     callback(null, deletedRecordsCount);
-  //   })
-  //   .catch((err) => {
-  //     callback(err);
-  //   })
-  // },
 
   deletePost(req, callback) {
     return Post.findById(req.params.id)
@@ -42,9 +46,9 @@ module.exports = {
 
         if (authorized) {
           post.destroy()
-          .then((deletedRecordsCount) => {
-            callback(null, deletedRecordsCount);
-          });
+            .then((deletedRecordsCount) => {
+              callback(null, deletedRecordsCount);
+            });
 
         } else {
           req.flash("notice", "You are not authorized to do that.")
@@ -55,25 +59,6 @@ module.exports = {
         callback(err);
       });
   },
-
-  // updatePost(id, updatedPost, callback){
-  //   return Post.findById(id)
-  //   .then((post) => {
-  //     if(!post){
-  //       return callback("Post not found");
-  //     }
-
-  //     post.update(updatedPost, {
-  //       fields: Object.keys(updatedPost)
-  //     })
-  //     .then(() => {
-  //       callback(null, post);
-  //     })
-  //     .catch((err) => {
-  //       callback(err);
-  //     });
-  //   });
-  // },
 
   updatePost(req, updatedPost, callback) {
     return Post.findById(req.params.id)
